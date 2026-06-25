74fa195ac3101ab697ba9e0c15496be2479cfb9a
Mohamed Faaris <mohamedfaaris.dev@gmail.com>
2026-06-25 19:38:42 +0530

    initial: arasi cafe coming soon page

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'arasi-cafe-v1';
+
+const assets = [
+  'index.html',
+  'manifest.json',
+  'favicon.ico',
+  'favicon-16x16.png',
+  'favicon-32x32.png',
+  'apple-touch-icon.png',
+  'android-chrome-192x192.png',
+  'android-chrome-512x512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(assets))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
